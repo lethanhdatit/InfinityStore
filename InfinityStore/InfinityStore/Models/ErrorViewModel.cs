4d53cd00672b12cdb276261185cf16546fd994ad
lethanhdatit <thanhdat.it.mmo@gmail.com>
2020-08-30 22:48:44 +0700

    fist commit

---
+++ InfinityStore/InfinityStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InfinityStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
