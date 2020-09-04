b2e01c336352785ab368bdd59f4c0c5a6518b8b8
lethanhdatit <thanhdat.it.mmo@gmail.com>
2020-09-04 17:26:34 +0700

    re-structure project + database designing

---
+++ InfinityStore/Infinity.Data/Entities/Identity/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Entities.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [MaxLength(50)]
+        [Required]
+        public string FName { get; set; }
+        [MaxLength(50)]
+        public string MName { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string LName { get; set; }
+        [Required]
+        public bool Gender { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
---
+++ InfinityStore/Infinity.Data/MyIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Data.Entities.Identity;
+namespace Data
+{
+    public class MyIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public MyIdentityDbContext(DbContextOptions<MyIdentityDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
